64d3d6b648bb57029c095a2f3fc63f3deb344333
Nicholas Mahbouby <nicholas.mahboubi@gmail.com>
2026-09-06 10:34:40 +0100

    Add HackerNewsClient

---
+++ src/BestNHackerNews/Services/HackerNewsClient.cs
@@ -0,0 +1,17 @@
+﻿using BestNHackerNews.Models;
+
+namespace BestNHackerNews.Services;
+
+public class HackerNewsClient(HttpClient httpClient) : IHackerNewsClient
+{
+    public async Task<int[]> GetBestStoryIdsAsync(CancellationToken cancellationToken)
+    {
+        var ids = await httpClient.GetFromJsonAsync<int[]>("beststories.json", cancellationToken);
+        return ids ?? [];
+    }
+
+    public Task<HackerNewsItem?> GetItemAsync(int id, CancellationToken cancellationToken)
+    {
+        return httpClient.GetFromJsonAsync<HackerNewsItem>($"item/{id}.json", cancellationToken);
+    }
+}
